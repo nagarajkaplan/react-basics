df2db45ab9fa6ed9a488e845294a6cd22ad4de0f
raj <nagaraja.kambathalli@kaplan.edu>
2024-02-26 11:36:48 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+// import ClassComponent from "./components/ClassComponent";
+// import FunctionalComponent from "./components/FunctionalComponent";
+
+import ParentComponent from "./components/Parent";
 
 function App() {
+  // const classHeading = "Class Component";
+  // const functionHeading = "Functional Component";
+
   return (
     <div>
       <h1>Hi Everyone </h1>
       <h1>Welcome to React js Session </h1>
+      {/* <ClassComponent heading={classHeading} /> */}
+      {/* <FunctionalComponent heading={functionHeading} /> */}
+      <ParentComponent />
     </div>
   );
 }
